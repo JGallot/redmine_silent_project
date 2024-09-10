d8abce0a6e02c097954cc4be0f57ecb7e69cff08
Jerome Gallot <gallot.jerome@unam-si.fr>
2024-09-10 09:53:06 +0200

    Deleting projet is now permitted..

--- db/migrate/001_create_project_silent.rb
+++ db/migrate/001_create_project_silent.rb
@@ -1,8 +1,8 @@
 class CreateProjectSilent < ActiveRecord::Migration[6.1]
   def change
     create_table :project_silents do |t|
-      t.references :project, index: true, foreign_key: true, null: false
-      t.references :tracker, index: true, foreign_key: true, null: false
+      t.references :project, index: true, foreign_key: { on_delete: :cascade}, null: false
+      t.references :tracker, index: true, foreign_key: { on_delete: :cascade}, null: false
       t.references :issue_status,  :column => :issue_status_id, index: true, null: false, foreign_key: true
     end
   end
