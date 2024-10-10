8d2c63da3a97ecc9f9754b59eb278d2abe42158d
Jerome Gallot <gallot.jerome@unam-si.fr>
2024-10-10 10:08:06 +0200

    New version

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_silent_project do
   name 'Redmine Silent Project plugin'
   author 'Jérôme Gallot'
   description 'This plugin provides functionality to disable notifications when changing status.'
-  version '0.2'
+  version '0.3'
   author_url 'https://github.com/JGallot'
 
   permission :admin_project_notifications, { project_notifications: [:index, :update] }
